f7fab47364524134299d06a47397a8d67644141f
samadani <blerckmermberh@gmail.com>
2026-07-02 21:58:18 +0100

    configure linters and fix styling and spacing issues

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly"
+      }
+    },
+    rules: {
+      "indent": ["error", 2],
+      "no-trailing-spaces": "error",
+      "no-use-before-define": "error"
+    }
+  }
+];
