2acac73ffe175d0b5a0cfba9121c81c1e867debe
samadani <blerckmermberh@gmail.com>
2026-07-03 09:07:31 +0100

    syntax corrections for eslint flat config and style formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
 module.exports = [
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs",
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
       globals: {
-        window: "readonly",
-        document: "readonly",
-        localStorage: "readonly"
-      }
+        window: 'readonly',
+        document: 'readonly',
+        localStorage: 'readonly',
+      },
     },
     rules: {
-      "indent": ["error", 2],
-      "no-trailing-spaces": "error",
-      "no-use-before-define": ["error", { "functions": false, "classes": true, "variables": true }]
-    }
-  }
+      indent: ['error', 2],
+      'no-trailing-spaces': 'error',
+      'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    },
+  },
 ];
